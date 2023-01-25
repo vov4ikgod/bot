9c305dac0e0751756ac97a9676c81c87e79029a7
Vladimir <86399820+vov4ikgod@users.noreply.github.com>
2023-01-25 22:46:52 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ function App() {
     return (
         <div className="App">
             <img src={'.//assets/logo.jpg'} className={'logo'}/>
+
+            <form>
+                <h1 className={'title'}>Оставить заявку</h1>
+
+                <input type="text" placeholder='Имя'></input>
+                <input type="text" placeholder='Телефон'></input>
+                <input type="text" placeholder='Возраст ребенка'></input>
+            </form>
         </div>
     );
 }
